0df20c2904060fbbfcef08eb256584fe7be4414e
unknown <cowang@CABBCNU1421PNR.ca.bestbuy.com>
2015-03-13 15:57:49 -0700

    new structure

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var app = require('express')();
+var express  = require('express');
+var app = express();
 var http = require('http').Server(app);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', function(req, res){
+  res.sendfile('./public/index.html');
 });
 
 http.listen(3000, function(){
