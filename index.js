f5414ea471384729156e9e423830d4ddb9bdf9d4
rex.wang <cowang@bestbuycanada.com>
2015-03-16 11:17:48 -0700

    change file serving code

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ var express  = require('express');
 var app = express();
 var http = require('http').Server(app);
 
-app.use(express.static(__dirname + '/public'));
-
 app.get('*', function(req, res){
-  res.sendfile('./public/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 http.listen(3000, function(){
